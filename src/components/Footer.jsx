9ca20796489605aadbb19bd6f5e5692b9361def4
Exodya21 <jaumegomiciai@gmail.com>
2022-03-02 12:04:29 +0100

    component Footer ready for work

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/index.css"
+
+function Footer() {
+    return (
+        <footer className="Footer">
+            <div>AssociatonsList</div>
+            <div>Information</div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,14 @@
-
 import '../styles/componets_styles/Layout.css';
 import Nav from "./Nav";
 import Main from './Main';
+import Footer from './Footer';
 
 function Layout() {
   return (
     <div className="Layout">
       <Nav />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
