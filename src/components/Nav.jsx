dd9182a5a767c49f4d86399c51711f6e4030b6db
Exodya21 <jaumegomiciai@gmail.com>
2022-03-07 13:34:45 +0100

    roting with viewCreate finish and also back to homePage and little styles from burger in nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,9 @@
 import "../styles/index.css";
+import burger from "../assets/burger.svg";
 import Searcher from "./dynamics_components/Searcher";
+
+import React from "react";
+import {Link} from "react-router-dom";
 
 function Nav() {
     return (
@@ -9,14 +13,23 @@ function Nav() {
 
             <header className="navbar">
                 <div>
-                    <img className="menu" src="../../assets/img/burger2.svg" alt="profile" />
+                    <img className="menu" src="../../assets/img/burger2.svg" alt="logoAssoc." />
                 </div>
                 <div className="dropdown">
                    
-                    <a href="#">name</a>
+                    <img src={burger} className="burger" alt="burger" />
                     <ul>
                         <li>
                             <a href="#">My Profile</a>
+                        </li>
+                        <li>
+                            <a href="#">Dashboard</a>
+                        </li>
+                        <li>
+                            <Link to="/createPost">Create new post</Link>
+                        </li>
+                        <li>
+                            <Link to="/">Home</Link>
                         </li>
                         <li>
                             <a href="#">Logout</a>
