c91feafca9300a008e2b729f758c12c41ed03f97
Davaious <davidalcalde.hyland@gmail.com>
2022-03-07 11:29:06 +0100

    petformulario jsx css

---
+++ src/components/dynamics_components/Petformulario.jsx
@@ -0,0 +1,20 @@
+import "../../styles/index.css";
+
+function Petformulario() {
+    return (
+        <div>
+            <Petformulario />
+            PetFormulario
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default Petformulario;
