bbe07b71852f825cd468c7490cad1311acf25098
Exodya21 <jaumegomiciai@gmail.com>
2022-03-07 12:03:42 +0100

    routing is working for a viewCreate

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import '../styles/componets_styles/Layout.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from "./Nav";
 import Main from './Main';
+import Petformulario from './dynamics_components/Petformulario';
 import Footer from './Footer';
 
 
@@ -12,19 +13,21 @@ function Layout() {
 
         <Routes>
           <Route path='/' element={<Nav />} />
+          <Route path='/createPost' element={<Nav />} />
 
         </Routes>
 
         <Routes>
           <Route path='/' element={<Main />} />
             {/* <Route index element={<DetailAssociation />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="createPost" element={<PetFormulario />} /> 
-            <Route path="*" element={<NoMatch />} /> */}
+            <Route path="dashboard" element={<Dashboard />} /> */}
+            <Route path="/createPost" element={<Petformulario />} /> 
+            {/* <Route path="*" element={<NoMatch />} /> */}
         </Routes>
 
         <Routes>
           <Route path='/' element={<Footer />} />
+          <Route path='/createPost' element={<Footer />} />
         </Routes>
 
       </BrowserRouter >
