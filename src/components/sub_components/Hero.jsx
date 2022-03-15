867925f76bfb6871a7e9a812eb1458ffe7c68fa5
unknown <patrasioo69@gmail.com>
2022-03-15 14:10:13 +0100

    I add button that anchors to the slider of Recent Pets Done

--- src/components/sub_components/Hero.jsx
+++ src/components/sub_components/Hero.jsx
@@ -1,4 +1,5 @@
 import "../../styles/index.css";
+import Flecha from "../../assets/img/flecha-_1_.svg"
 
 function Hero() {
     
@@ -6,6 +7,9 @@ function Hero() {
         <div className="hero">
             <div className="hero-info">
                 <h1 className="hero-title">Don't buy, Adopt.</h1>
+            </div>
+            <div className="hero-gradient">
+                <a href="#anchorHero" className="hero-button"><img className="hero-buttonImg" src={Flecha} alt="" /></a>
             </div>
         </div>
     )
