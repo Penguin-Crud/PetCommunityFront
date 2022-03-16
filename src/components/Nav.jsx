86f1e9ba0d8dfecf3c98768bdef03a83c3d22531
unknown <patrasioo69@gmail.com>
2022-03-16 12:33:43 +0100

    added the logo and the browser input in the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import "../styles/index.css";
-import burger from "../assets/burger.svg";
 import Searcher from "./dynamics_components/Searcher";
+import Usuario from "../assets/usuario.png";
+import Logo from "../assets/logo2.svg";
 
 import React from "react";
 import {Link} from "react-router-dom";
@@ -8,16 +9,18 @@ import {Link} from "react-router-dom";
 function Nav() {
     return (
         <nav className="nav">
-            <p>Logo</p>
-            <Searcher />
+            <div className="logo-container">
+                <img className="logo" src={Logo} alt="" />
+            </div>
+            <div className="container-searcher">
+                <Searcher />
+            </div>
 
             <header className="navbar">
-                <div>
-                    <img className="menu" src="../../assets/img/burger2.svg" alt="logoAssoc." />
-                </div>
-                <div className="dropdown">
-                   
-                    <img src={burger} className="burger" alt="burger" />
+                <div className="dropdown"> 
+                    <div className="container-usuario">
+                        <img src={Usuario} className="burger" alt="burger" />
+                    </div>
                     <ul>
                         <li>
                             <a href="#">My Profile</a>
@@ -34,8 +37,7 @@ function Nav() {
                         <li>
                             <a href="#">Logout</a>
                         </li>
-                    </ul>
-                        
+                    </ul>         
                 </div>
             </header>
         
