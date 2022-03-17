8bb33405bd1f9aaab4a983b73725049490784983
timfel20 <timmyfelicia@gmail.com>
2022-03-17 12:20:03 +0100

    slider made responsive

--- src/components/cards/PetCard.jsx
+++ src/components/cards/PetCard.jsx
@@ -3,7 +3,9 @@ import "../../styles/index.css";
 function PetCard({name, years, date, imgURL}) {
     return (
         <div className="cardPet">
-            <img className="imgPet" src={imgURL} alt="joto" />
+            <div className="slider-img-container">
+               <img className="imgPet" src={imgURL} alt="joto" />
+            </div>
             <div className="infoPet">    
                 <div>
                     <p>{name}</p>
