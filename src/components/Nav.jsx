ffe195c9a532e275338913545234847254937401
unknown <patrasioo69@gmail.com>
2022-03-21 10:53:13 +0100

    styles changes in the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,20 @@
 import "../styles/index.css";
 import Searcher from "./dynamics_components/Searcher";
-import Usuario from "../assets/usuario.png";
+//import Usuario from "../assets/usuario.png";
+
 import Logo from "../assets/logo2.svg";
 
 import React from "react";
 import {Link} from "react-router-dom";
 
 function Nav() {
+
+    let id = 0;
+
     return (
         <nav className="nav">
             <div className="logo-container">
-                <img className="logo" src={Logo} alt="logo" />
+                <Link to="/"> <img className="logo" src={Logo} alt="logo" /> </Link>
             </div>
             <div className="container-searcher">
                 <Searcher />
@@ -19,11 +23,11 @@ function Nav() {
             <header className="navbar">
                 <div className="dropdown"> 
                     <div className="container-usuario">
-                        <img src={Usuario} className="burger" alt="burger" />
+                        <img src="https://d1kvlp4er3agpe.cloudfront.net/resources/images/groups/3/6/3/2/1/53fzpnfuwu.jpg" className="burger" alt="burger" />
                     </div>
                     <ul>
                         <li>
-                            <Link to="/associtionDetail">My Profile</Link>
+                            <Link to={`/detailAssociation/${id}`}>My Profile</Link>
                         </li>
                         <li>
                             <Link to="/dashboard">Dashboard</Link>
