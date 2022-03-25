2e87478c2e90880a46a4699319a196648f7fcc6c
Exodya21 <jaumegomiciai@gmail.com>
2022-03-25 19:46:33 +0100

    clean code

--- src/components/dynamics_components/lists/Dashboard.jsx
+++ src/components/dynamics_components/lists/Dashboard.jsx
@@ -27,69 +27,6 @@ function Dashboard() {
         setDataPets(arrayFiltrado)
         deleteById("/pets", id);
     }
-
-    /* return (
-    <div className="containerDashboard">
-        <h1 className="titleSlider marginTitle">Dashboard</h1>
-        
-        <div className="tableContainer">
-            <table >
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Date</th>
-                        <th>Species</th>
-                        <th>Race</th>
-                        <th>Gender</th>
-                        <th>Size</th>
-                        <th>Vaccines</th>
-                        <th>Chip</th>
-                        <th className="dashboardActions">
-                            <div className="leftBorder"></div>
-                            Actions
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    dataExist? dataPets.map(pet => {
-                        return <PetCardDashboard 
-                                    key={pet.id}
-                                    id={pet.id}
-                                    name={pet.name}
-                                    age={pet.age}
-                                    date={pet.date}
-                                    specie={pet.specie}
-                                    race={pet.race}
-                                    gender={pet.gender}
-                                    size={pet.size}
-                                    vaccines={pet.vaccines}
-                                    chip={pet.chip}
-                                    deletePet={deletePet}
-                               />
-                               
-                    })
-                    :<Loading/>
-                    
-                }
-                </tbody>
-            </table>
-        </div>
-        <div className="btnAddPet">
-            <Link to="/createPost"> <button> <p>Add Pet</p> </button> </Link>
-        </div>
-    </div>
-    
-    
-    )
-}
-
-export default Dashboard; 
- */
-
-
  return (
     <div className="container-dashboard">
         <h1 className="title-dashboard">Dashboard</h1>
