11597bb1f583b8be1f16333b7a9d70694553b45f
unknown <patrasioo69@gmail.com>
2022-03-26 16:37:53 +0100

    data auxiliar justify in footer

--- src/components/dynamics_components/lists/AssociationsList.jsx
+++ src/components/dynamics_components/lists/AssociationsList.jsx
@@ -3,11 +3,22 @@ import AssociationCard from "../../cards/AssociationCard";
 import { useEffect, useState } from "react";
 import { dataPetsService } from "../../../services/PetCommunityServices";
 import Loading from "../Loading";
+import { isOnline } from "../../../services/PetCommunityServices";
 
 function AssociationsList() {
 
     const [dataExist, setDataExist] = useState(false);
     const [dataAssociations, setDataAssociations] = useState()
+    const [isOnlinee, setIsOnlinee] = useState(true);
+
+    isOnline()
+        .then(res => {
+            // console.log("before: ", isOnlinee)
+            setIsOnlinee(res)
+            // console.log("after: ", isOnlinee)
+        }
+    )
+
 
     useEffect( () =>{
 
@@ -21,16 +32,21 @@ function AssociationsList() {
     return (
         <div className="cardListAssociation">
             {
-                dataExist ? dataAssociations.map(association => {
-                    return <div className="containerBars" key={association.id}>
-                                <div className="bars"></div>
-                                <AssociationCard 
-                                    imgURL={association.logo}
-                                    id={association.id}
-                                />
-                                <div className="bars"></div>
-                           </div>
-                })
+                dataExist ?
+                    isOnlinee ?
+                        dataAssociations.map(association => {
+                            return <div className="containerBars" key={association.id}>
+                                        <div className="bars"></div>
+                                        <AssociationCard 
+                                            imgURL={association.logo}
+                                            id={association.id}
+                                        />
+                                        <div className="bars"></div>
+                                    </div>
+                }):
+                                    <div className="container-img-data-auxiliar-aso"> 
+                                        <img src={"https://i.pinimg.com/236x/6b/22/98/6b2298fec93ad8240f87c8228ab87969.jpg"} alt="img data auxiliar" />
+                                    </div> 
                 :
                 <Loading/>
             }
