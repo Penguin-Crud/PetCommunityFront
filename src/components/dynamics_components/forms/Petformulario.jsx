9664d444c835da470bd5b8be5177fdd84804c14c
Exodya21 <jaumegomiciai@gmail.com>
2022-03-26 20:50:39 +0100

    form styles missing mediaquerys

--- src/components/dynamics_components/forms/Petformulario.jsx
+++ src/components/dynamics_components/forms/Petformulario.jsx
@@ -18,9 +18,9 @@ function Petformulario() {
     const [description, setDescription] = useState('');
     const [vaccines, setVaccines] = useState(false);
     const [chip, setChip] = useState(false);
-    const [date, setDate] = useState('');
+    const [priority, setPriority] = useState('');
     
-    let list = [name, age, gender, /*location,*/ size, specie, race, description, vaccines, chip, date]
+    let list = [name, age, gender, /*location,*/ size, specie, race, description, vaccines, chip, priority]
     let formData = new FormData();
     
     const cleanInputs = (event) => {
@@ -40,7 +40,7 @@ function Petformulario() {
      //      ',\n    "description": "muy cariñoso y bueno"',
      //      ',\n    "vaccines": true',
      //      ',\n    "chip": true',
-     //      ',\n    "date": "15/02/2020"',
+     //      ',\n    "priority": "15/02/2020"',
      //      '\n   }\n'
      // ]
      let datas = [
@@ -53,7 +53,7 @@ function Petformulario() {
           ',\n    "description": ',
           ',\n    "vaccines": ',
           ',\n    "chip": ',
-          ',\n    "date": ',
+          ',\n    "priority": ',
           '\n   }\n'
      ]
     
@@ -77,142 +77,149 @@ function Petformulario() {
     }
 
     return (
-        <div> 
-            <form name='formCreate' className="FormPet" onSubmit={ addData }> 
-               <h1 className="titleFormCreate"> New Pet</h1>
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Name"
-                    defaultValue=""
-                    name="name"
-                    onChange={ (e) => setName(e.target.value) }
-               />
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Age" 
-                    defaultValue=""
-                    age="age"
-                    onChange={ (e) => setAge(e.target.value) }
-               />
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Date" 
-                    defaultValue=""
-                    date="date"
-                    onChange={ (e) => setDate(e.target.value) }
-               />
-               <p>{gender}</p>
-              <div> 
-                   <label htmlFor='male'>Male</label>
-                    <input 
-                    type="radio"
-                    gender="gender"
-                    name= "gender"
-                    onClick={ () => setGender("male")}
-                    
-                   />
-               </div>
-
-                <div>
-                <label htmlFor='female'>Female</label>
-                    <input 
-                    type="radio"
-                    gender="gender"
-                    name= "gender"
-                    onClick={ () => setGender("female")}
-                    
-                   />
-                </div>
-               
-
-               {/* <input 
-                    type="text"
-                    required 
-                    placeholder="Location" 
-                    defaultValue=""
-                    location="location"
-                    onChange={ (e) => setLocation(e.target.value) }
-               /> */}
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Size(small,medium,tall)" 
-                    defaultValue=""
-                    size="size"
-                    onChange={ (e) => setSize(e.target.value) }
-               />
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Species(Dog-Cat)" 
-                    defaultValue=""
-                    specie="specie"
-                    onChange={ (e) => setSpecie(e.target.value) }
-               />
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Race" 
-                    defaultValue=""
-                    race="race"
-                    onChange={ (e) => setRace(e.target.value) }
-               />
-               <input 
-                    type="text"
-                    required 
-                    placeholder="Description" 
-                    defaultValue=""
-                    description="description"
-                    onChange={ (e) => setDescription(e.target.value) }
-               />
+        <div className='container-form-create'> 
+          <h1 className="title-register">New Pet</h1>
+          <form name='formCreate' className="formCreate" onSubmit={ addData }> 
                <div>
-                    <div>
-                         <label htmlFor="vaccines">Vaccined</label>
-                         <input 
-                              type="checkbox" 
-                              defaultChecked={vaccines} 
-                              id='vaccines'
-                              name="vaccines"
-                              vaccines="vaccines"
-                              onClick={ vaccines? () => setVaccines(false) : () => setVaccines(true) }
-                         />
-                         {/* {console.log(vaccines)} */}
-                    </div>    
-                    <div>
-                         <label htmlFor="chip">Chip</label>
-                         <input 
-                              type="checkbox" 
-                              defaultChecked={chip} 
-                              id='chip'
-                              name="chip"
-                              chip="chip"
-                              onClick={ chip? () => setChip(false) : () => setChip(true) }
-                         />
-                         {/* {console.log(chip)} */}
+                    <div className='form-top'>
+                         <div className='name-age-description-priority'>
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Name"
+                                   defaultValue=""
+                                   name="name"
+                                   onChange={ (e) => setName(e.target.value) }
+                              />
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Age" 
+                                   defaultValue=""
+                                   age="age"
+                                   onChange={ (e) => setAge(e.target.value) }
+                                   />
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Description" 
+                                   defaultValue=""
+                                   description="description"
+                                   onChange={ (e) => setDescription(e.target.value) }
+                                   />
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Priority for adoptation" 
+                                   defaultValue=""
+                                   priority="priority"
+                                   onChange={ (e) => setPriority(e.target.value) }
+                              />
+
+                              <input 
+                                   id='input-fileImg'
+                                   type="file"
+                                   conte
+                                   accept="image/*"
+                                   required 
+                                   placeholder="Picture" 
+                                   defaultValue=""
+                                   picture="picture"
+                                   onChange={ (e) => setPicture(e.target.files[0]) }
+                              />
+                              <label htmlFor='input-fileImg' className='button-file'>Select Image</label>
+                         </div>
+                         <div className='size-species-race-gender-vachip' >
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Size(small,medium,tall)" 
+                                   defaultValue=""
+                                   size="size"
+                                   onChange={ (e) => setSize(e.target.value) }
+                              />
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Species(Dog-Cat)" 
+                                   defaultValue=""
+                                   specie="specie"
+                                   onChange={ (e) => setSpecie(e.target.value) }
+                              />
+                              <input 
+                                   type="text"
+                                   required 
+                                   placeholder="Race" 
+                                   defaultValue=""
+                                   race="race"
+                                   onChange={ (e) => setRace(e.target.value) }
+                              />
+                              <div className='gender'>
+                                   <div> 
+                                        <label htmlFor='male'>Male</label>
+                                        <input 
+                                        type="radio"
+                                        gender="gender"
+                                        name= "gender"
+                                        onClick={ () => setGender("male")}     
+                                        />
+                                   </div>
+                                   <div>
+                                        <label htmlFor='female'>Female</label>
+                                        <input 
+                                        type="radio"
+                                        gender="gender"
+                                        name= "gender"
+                                        onClick={ () => setGender("female")}
+                                        />
+                                   </div>
+                              </div>
+                              <div className='vachip'>
+                                   <div >
+                                        <label htmlFor="vaccines">Vaccined</label>
+                                        <input 
+                                             type="checkbox" 
+                                             defaultChecked={vaccines} 
+                                             id='vaccines'
+                                             name="vaccines"
+                                             vaccines="vaccines"
+                                             onClick={ vaccines? () => setVaccines(false) : () => setVaccines(true) }
+                                        />
+                                        {/* {console.log(vaccines)} */}
+                                   </div>    
+                                   <div >
+                                        <label htmlFor="chip">Chip</label>
+                                        <input 
+                                             type="checkbox" 
+                                             defaultChecked={chip} 
+                                             id='chip'
+                                             name="chip"
+                                             chip="chip"
+                                             onClick={ chip? () => setChip(false) : () => setChip(true) }
+                                        />
+                                        {/* {console.log(chip)} */}
+                                   </div>
+                              </div>
+                         </div>
                     </div>
+                    {/* <input 
+                         type="text"
+                         required 
+                         placeholder="Location" 
+                         defaultValue=""
+                         location="location"
+                         onChange={ (e) => setLocation(e.target.value) }
+                    /> */}
+                    
+                   
+                    <button type="sumbit">Submit</button>
+                 
                </div>
-               <input 
-                    id='input-fileImg'
-                    type="file"
-                    conte
-                    accept="image/*"
-                    // required 
-                    placeholder="Picture" 
-                    defaultValue=""
-                    picture="picture"
-                    onChange={ (e) => setPicture(e.target.files[0]) }
-               />
-               <label htmlFor='input-fileImg' className='button-file'>Select Image</label>
-               <div>
-                 <button type="sumbit">Submit</button>
+               <div className='DogAndCat' >
+                    <img className="imgDogAndCat" src={dogcat} alt="dogcat"/>
                </div>
-               
-            </form>
+          </form>
 
-            <img className="imgDogAndCat" src={dogcat} alt="dogcat"/>
             
         </div>
     )
